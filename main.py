f7e3ed738f6468249ba4bad6b39f57b3bd2d18cd
Kedar154 <hadnurkarkedar@gmail.com>
2026-03-28 15:57:48 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from data_and_nodes.gamestate import state
+from data_and_nodes.gamestate import State
 from data_and_nodes.retrieval_lie_detection import retrieval, detect_lie
 from data_and_nodes.llms import speed, conv
-from data_and_nodes.summarizer import state
-#from data_and_nodes.sus import state
+from data_and_nodes.summarizer import summarization_node
+from data_and_nodes.interaction import prompt_repsonse
+#from data_and_nodes.sus_score import sus
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.types import interrupt
+from langchain_core.tools import tool
+#from langchain_core.messages import HumanMessage, SystemMessage
+#from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+#from sentence_transformers import SentenceTransformer
+from neo4j import GraphDatabase
+from typing import TypedDict, Annotated
+import numpy as np
+import uuid
+import os
+
+garph = StateGraph(State)
+garph.add_node('retrieve', retrieval)
+garph.add_node('lie_detection', detect_lie)
+#garph.add_node('sus', sus)
+garph.add_node('prompt_response', prompt_repsonse)
+garph.add_node('summary', summarization_node)
+
+
